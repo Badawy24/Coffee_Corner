ee8cef991dfcd4545904320076fc798481e40684
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2025-05-02 20:40:51 +0300

    Footer Section

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,12 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks = document.querySelectorAll(".nav-link");
 
 menuOpenButton.addEventListener("click",()=>{
     document.body.classList.toggle("show-mobile-menu")
 });
 menuCloseButton.addEventListener("click",()=>menuOpenButton.click());
+
+navLinks.forEach(link=>{
+    link.addEventListener("click",()=> menuOpenButton.click());
+});
